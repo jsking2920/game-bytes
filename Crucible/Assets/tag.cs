97a1e473edf6ddedd805df8fe19f9079d12bc3ca
Scott King <71986291+jsking2920@users.noreply.github.com>
2020-10-09 15:11:16 -0400

    Tag Mechanic v0.1
    Wrote script for tag mechanic, renamed some files, trying to fix collision detection bug

---
+++ Crucible/Assets/tag.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Versioning;
+using UnityEngine;
+
+public class tag : MonoBehaviour
+{
+    private bool isTagged = false;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    //string nameOfSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /*
+    void onCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //nameOfSprite = col.gameObject.GetComponent<SpriteRender>().sprite.name;
+        }
+    }
+    */
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            sr.sprite = (Sprite)Resources.Load<Sprite>("TowerfallTag/TempAssets/WhiteSquare.png");
+        }
+    }
+}
