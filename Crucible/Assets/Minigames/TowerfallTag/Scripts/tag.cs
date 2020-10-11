60e119579c9765182585e12e10e59767a7bb9cf3
Nicholas Hungria <nickhungria@outlook.com>
2020-10-11 15:29:37 -0400

    Add Win Conditions

--- Crucible/Assets/Minigames/TowerfallTag/Scripts/tag.cs
+++ Crucible/Assets/Minigames/TowerfallTag/Scripts/tag.cs
@@ -12,17 +12,27 @@ public class tag : MonoBehaviour
     public Sprite taggedSprite;
     public Sprite notTaggedSprite;
 
+    int playerNumber;
+
     //this players sprite renderer component
     SpriteRenderer thisSpriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Get the player number from the MovementController script
+        playerNumber = this.gameObject.GetComponent<MovementController>().playerNumber;
+
         thisSpriteRenderer = GetComponent<SpriteRenderer>();
         //sets the player that starts tagged to the appropriate sprite
         if (isTagged)
         {
             thisSpriteRenderer.sprite = taggedSprite;
+        }
+        else 
+        {
+            //Set the player's score to 1 since they're currently winning
+            MinigameController.Instance.AddScore(playerNumber, 1);
         }
     }
 
@@ -33,11 +43,13 @@ public class tag : MonoBehaviour
         {
             thisSpriteRenderer.sprite = taggedSprite;
             isTagged = true;
+            MinigameController.Instance.AddScore(playerNumber, -1);
         }
         //the tagged player should become untagged
         else if (col.gameObject.tag == "Player" && isTagged){
             thisSpriteRenderer.sprite = notTaggedSprite;
             isTagged = false;
+            MinigameController.Instance.AddScore(playerNumber, 1);
         }
  
     }
