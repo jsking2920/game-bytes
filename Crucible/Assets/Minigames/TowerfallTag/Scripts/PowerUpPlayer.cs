ce5d71905310237159a49c01df9c3b51036bdf7f
actuallyatoaster <7093583+actuallyatoaster@users.noreply.github.com>
2020-11-08 17:06:48 -0500

    Add power up text

--- Crucible/Assets/Minigames/TowerfallTag/Scripts/PowerUpPlayer.cs
+++ Crucible/Assets/Minigames/TowerfallTag/Scripts/PowerUpPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PowerUpPlayer : MonoBehaviour
 {
@@ -8,20 +9,25 @@ public class PowerUpPlayer : MonoBehaviour
     //Store current power up; 0 = None, 1 = Speed Boost, 2 = Jump Boost
     public float powerUpSpeedScalar = 2;
     public float powerUpJumpScalar = 2;
+    public TextMeshProUGUI powerUpText;
+
 
     MovementController movementController;
+    int playerNumber;
 
     // Start is called before the first frame update
     void Start()
     {
         //Get the movement controller script
         movementController = this.gameObject.GetComponent<MovementController>();
+        playerNumber = movementController.playerNumber;
+        powerUpText.SetText("");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void setPowerUp(int powerUpType)
@@ -29,23 +35,29 @@ public class PowerUpPlayer : MonoBehaviour
         switch (powerUpType)
         {
             case 1:
-                movementController.setMoveSpeed(movementController.defaultMoveSpeed*powerUpSpeedScalar);
+                movementController.setMoveSpeed(movementController.defaultMoveSpeed * powerUpSpeedScalar);
+                powerUpText.SetText("Speed boost!");
                 break;
             case 2:
                 movementController.setJumpForce(movementController.defaultJumpForce * powerUpJumpScalar);
+                powerUpText.SetText("Jump Boost!");
                 break;
             case 3:
                 movementController.hasDoubleJump = true;
+                powerUpText.SetText("Double Jump!");
                 break;
             case 4:
                 movementController.hasJetPack = true;
+                powerUpText.SetText("Jet pack!");
                 break;
             case 5:
                 movementController.hasDash = true;
+                powerUpText.SetText("Dash!");
                 break;
             default:
                 break;
         }
+
     }
 
     //Reset power ups
@@ -56,5 +68,6 @@ public class PowerUpPlayer : MonoBehaviour
         movementController.hasDoubleJump = false;
         movementController.hasJetPack = false;
         movementController.hasDash = false;
+        powerUpText.SetText("");
     }
 }
