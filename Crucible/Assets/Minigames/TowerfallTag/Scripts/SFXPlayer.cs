be1fe7c75c7cb46ea9fdf894a5d30273f0ef78b4
Nate-TheBeefCloud <71986228+Nate-TheBeefCloud@users.noreply.github.com>
2020-11-15 14:49:47 -0500

    added sound effects

---
+++ Crucible/Assets/Minigames/TowerfallTag/Scripts/SFXPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXPlayer : MonoBehaviour
+{
+    //create variable for each sound effect
+    public AudioSource Crash1;
+    public AudioSource Menu1;
+
+    //function to play each sound effect
+    public void PlayCrash1()
+    {
+        Crash1.Play();
+    }
+
+    public void PlayMenu1()
+    {
+        Menu1.Play();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //play sound when touch wall/floor
+        if (col.gameObject.tag == "wall")
+        {
+            Crash1.Play();
+        }
+        //play sound when touch player
+        else if (col.gameObject.tag == "Player")
+        {
+            Menu1.Play();
+        }
+
+    }
+}
