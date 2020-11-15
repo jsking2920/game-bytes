670268023b90afab336d73dcfe348cae27de2808
Scott King <71986291+jsking2920@users.noreply.github.com>
2020-11-15 15:35:06 -0500

    Refactored Sound Effects

--- Crucible/Assets/Minigames/TowerfallTag/Scripts/SFXPlayer.cs
+++ Crucible/Assets/Minigames/TowerfallTag/Scripts/SFXPlayer.cs
@@ -5,31 +5,21 @@ using UnityEngine;
 public class SFXPlayer : MonoBehaviour
 {
     //create variable for each sound effect
-    public AudioSource Crash1;
-    public AudioSource Menu1;
+    public AudioSource PlatformCollision;
+    public AudioSource PlayerCollision;
 
-    //function to play each sound effect
-    public void PlayCrash1()
-    {
-        Crash1.Play();
-    }
-
-    public void PlayMenu1()
-    {
-        Menu1.Play();
-    }
-
+    
     void OnCollisionEnter2D(Collision2D col)
     {
         //play sound when touch wall/floor
-        if (col.gameObject.tag == "wall")
+        if (col.gameObject.tag == "wall" && col.relativeVelocity.magnitude > 2)
         {
-            Crash1.Play();
+            PlatformCollision.Play();
         }
         //play sound when touch player
         else if (col.gameObject.tag == "Player")
         {
-            Menu1.Play();
+            PlayerCollision.Play();
         }
 
     }
