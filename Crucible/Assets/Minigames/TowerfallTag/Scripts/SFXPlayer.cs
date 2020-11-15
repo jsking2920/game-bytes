44a3ec279090016cec047d7aedb109b70ee7744d
Scott King <71986291+jsking2920@users.noreply.github.com>
2020-11-15 16:16:45 -0500

    Tweaked SFX
    Made it so platform collision sound only plays when player is moving downward fast enough and only if they are above the platform to prevent sound from playing when jumping through a platform.

--- Crucible/Assets/Minigames/TowerfallTag/Scripts/SFXPlayer.cs
+++ Crucible/Assets/Minigames/TowerfallTag/Scripts/SFXPlayer.cs
@@ -7,14 +7,30 @@ public class SFXPlayer : MonoBehaviour
     //create variable for each sound effect
     public AudioSource PlatformCollision;
     public AudioSource PlayerCollision;
+    Rigidbody2D thisRigidBody;
+    bool isFalling;
 
-    
+    void Start()
+    {
+        thisRigidBody = GetComponent<Rigidbody2D>();
+        isFalling = false;
+    }
+
+    void Update()
+    {
+        if (thisRigidBody.velocity.y < -5)
+        {
+            isFalling = true;
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D col)
     {
         //play sound when touch wall/floor
-        if (col.gameObject.tag == "wall" && col.relativeVelocity.magnitude > 2)
+        if (col.gameObject.tag == "wall" && isFalling && transform.position.y > col.gameObject.transform.position.y + 0.75f)
         {
             PlatformCollision.Play();
+            isFalling = false;
         }
         //play sound when touch player
         else if (col.gameObject.tag == "Player")
