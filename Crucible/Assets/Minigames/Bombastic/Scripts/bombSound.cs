7e461c9261b45e7f3b93c1284265b37f91410bae
Scott King <71986291+jsking2920@users.noreply.github.com>
2020-11-29 16:13:24 -0500

    Bomb sfx fix

--- Crucible/Assets/Minigames/Bombastic/Scripts/bombSound.cs
+++ Crucible/Assets/Minigames/Bombastic/Scripts/bombSound.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class bombSound : MonoBehaviour
 {
+    private bool hasPlayed = false;
     public AudioSource explosion;
 
     // Start is called before the first frame update
@@ -15,8 +16,9 @@ public class bombSound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (MinigameController.Instance.GetElapsedTime() > 4.9)//9.9)
+        if (MinigameController.Instance.GetElapsedTime() > 59.9 && !hasPlayed)
         {
+            hasPlayed = true;
             explosion.Play();
         }
     }
